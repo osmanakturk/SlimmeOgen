c4b3c5357f62a23c5499ba7185c0e2ddd7859df8
Osman <osmanaktrk@gmail.com>
2025-04-16 01:44:26 +0200

    Added necessary functions to display the camera.

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, Response
+from modules import *
+import cv2
 
 app = Flask(__name__)
+
+def gen_frames():
+    cap = cv2.VideoCapture(0)
+    while True:
+        success, frame = cap.read()
+        if not success:
+            break
+        _, buffer = cv2.imencode('.jpg', frame)
+        frame = buffer.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
 
 @app.route('/')
 def home():
@@ -9,6 +23,12 @@ def home():
 @app.route('/functie/<naam>')
 def functie(naam):
     return render_template('result.html', functie=naam)
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
